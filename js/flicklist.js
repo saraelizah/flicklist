5f2a00e173f47cc5f9f2fdb41cd2e5080192a356
Sara Howard <howard.sarae@gmail.com>
2019-07-02 19:40:51 -0500

    finish Flicklist 1 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,71 +1,98 @@
-
-
 var model = {
-  watchlistItems: [],
-  browseItems: []
+	watchlistItems: [],
+	browseItems: []
+  }
+  
+  
+  var api = {
+	root: "https://api.themoviedb.org/3",
+	token: "ec8410cfbcf3c47605a2d5e72ff77e2a" 
+	
+	// DONE / TODO 0 put your api key here
+  
 }
-
-
-var api = {
-  root: "https://api.themoviedb.org/3",
-  token: "TODO" // TODO 0 put your api key here
-}
-
-
-/**
- * Makes an AJAX request to themoviedb.org, asking for some movies
- * if successful, updates the model.browseItems appropriately, and then invokes
- * the callback function that was passed in
- */
-function discoverMovies(callback) {
-	$.ajax({
-		url: api.root + "/discover/movie",
-		data: {
-			api_key: api.token,
-		},
-		success: function(response) {
-			console.log("We got a response from The Movie DB!");
-			console.log(response);
+  
+  
+  /**
+   * Makes an AJAX request to themoviedb.org, asking for some movies
+   * if successful, updates the model.browseItems appropriately, and then invokes
+   * the callback function that was passed in
+   */
+  
+   function discoverMovies(callback) {
+	  $.ajax({
+		  url: api.root + "/discover/movie",
+		  data: {
+			  api_key: api.token,
+		  },
+		  success: function(response) {
+			  console.log("We got a response from The Movie DB!");
+			  console.log(response);
+			  
+			  // TODO 2
+			  // update the model, setting its .browseItems property equal to the movies we recieved in the response
 			
-			// TODO 2
-			// update the model, setting its .browseItems property equal to the movies we recieved in the response
-			
-			// invoke the callback function that was passed in. 
-			callback();
-		}
+			  model.browseItems = response.results;
+
+			  // invoke the callback function that was passed in. 
+
+			  callback();
+		  }
+	  });
+  }
+  
+  
+  /**
+   * re-renders the page with new content, based on the current state of the model
+   */
+
+  function render() {
+
+	// TODO 7
+	// clear everything from both lists
+
+	$("#section-watchlist ul").empty();
+	$("#section-browse ul").empty();
+	
+	// TODO 6
+	// for each movie on the user's watchlist, insert a list item into the <ul> in the watchlist section
+
+	model.watchlistItems.forEach(function(movie) {
+		var watchlist = $("<li></li>").text(movie.title);
+		$("#section-watchlist ul").append(watchlist);
 	});
-  
-}
+	
+	// for each movie on the current browse list, 
 
+	model.browseItems.forEach(function(movie) {
 
-/**
- * re-renders the page with new content, based on the current state of the model
- */
-function render() {
-  // TODO 7
-  // clear everything from both lists
+		  // DONE / TODO 3
+		  // insert a list item into the <ul> in the browse section
+
+		  var movieTitle = $("<p></p>").text(movie.title);
+		  var browse = $("<li></li>").append(movieTitle)
+		  $("#section-browse ul").append(browse);
+		  
+		  // DONE / TODO 4
+		  // the list item should include a button that says "Add to Watchlist"
+
+		  var button = $("<button></button>").text("Add to Watchlist").click(function(){
+			model.watchlistItems.push(movie);
+			render();
+		  });
+		  browse.append(button)
+		  
+		  // DONE / TODO 5
+		  // when the button is clicked, this movie should be added to the model's watchlist and render() should be called again
+	
+	});
+	
+  }
   
-  // TODO 6
-  // for each movie on the user's watchlist, insert a list item into the <ul> in the watchlist section
   
-  // for each movie on the current browse list, 
-  model.browseItems.forEach(function(movie) {
-		// TODO 3
-		// insert a list item into the <ul> in the browse section
-		
-		// TODO 4
-		// the list item should include a button that says "Add to Watchlist"
-		
-		// TODO 5
-		// when the button is clicked, this movie should be added to the model's watchlist and render() should be called again
+  // When the HTML document is ready, we call the discoverMovies function,
+  // and pass the render function as its callback
+  $(document).ready(function() {
+	discoverMovies(render);
   });
   
-}
-
-
-// When the HTML document is ready, we call the discoverMovies function,
-// and pass the render function as its callback
-$(document).ready(function() {
-  discoverMovies(render);
-});
-
