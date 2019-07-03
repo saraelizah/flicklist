52fe61e466792b237d6f5794e031ed6aac633d1c
Sara Howard <howard.sarae@gmail.com>
2019-07-02 22:48:21 -0500

    finish Flicklist 2 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -14,7 +14,8 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "8e888fa39ec243e662e1fb738c42ae99" // TODO 0 add your api key
+  token: "ec8410cfbcf3c47605a2d5e72ff77e2a" 
+  // DONE / TODO 0 add your api key
 }
 
 
@@ -45,7 +46,19 @@ function discoverMovies(callback) {
  * if successful, updates model.browseItems appropriately and then invokes
  * the callback function that was passed in
  */
+
 function searchMovies(searchTerm, callback) {
+  $.ajax({
+    url: api.root + "/search/movie",
+    data: {
+      api_key: api.token,
+      query: searchTerm
+    },
+    success: function(response) {
+      model.browseItems = response.results;
+      callback(response);
+    }
+  })
   console.log("searching for movies with '" + searchTerm + "' in their title...");
 
   // TODO 9
@@ -59,6 +72,7 @@ function searchMovies(searchTerm, callback) {
 /**
  * re-renders the page with new content, based on the current state of the model
  */
+
 function render() {
 
   // clear everything
@@ -70,8 +84,11 @@ function render() {
     var title = $("<p></p>").text(movie.original_title);
     var itemView = $("<li></li>")
       .append(title)
-      // TODO 3
+
+      // DONE / TODO 3 
       // give itemView a class attribute of "item-watchlist"
+
+      .attr("class", "item-watchlist");    
 
     $("#section-watchlist ul").append(itemView);
   });
@@ -84,23 +101,29 @@ function render() {
       .click(function() {
         model.watchlistItems.push(movie);
         render();
-      });
-      // TODO 2
+      })
+      
+      // DONE / TODO 2
       // the button should be disabled if this movie is already in
       // the user's watchlist
       // see jQuery .prop() and Array.indexOf()
 
+      .prop("disabled", model.watchlistItems.indexOf(movie) !== -1);
 
-    // TODO 1
+    // DONE / TODO 1
     // create a paragraph containing the movie object's .overview value
     // then, in the code block below,
     // append the paragraph in between the title and the button
 
+    var overview = $("<p></p>").text(movie.overview);  
+
 
     // append everything to itemView, along with an <hr/>
+
     var itemView = $("<li></li>")
       .append($("<hr/>"))
       .append(title)
+      .append(overview)
       .append(button);
 
     // append the itemView to the list
